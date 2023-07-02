c31ce27570510eb168f11f96f280bc9ab94cf544
AwesomeJim <jimnahgua@gmail.com>
2023-07-02 12:08:05 +0300

    Changes: converting the App to Compose: fixed scroll when device is rotated

--- app/src/main/java/com/jim/countdowntimer/ui/HomeScreen.kt
+++ app/src/main/java/com/jim/countdowntimer/ui/HomeScreen.kt
@@ -15,7 +15,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
@@ -176,7 +178,7 @@ fun TokenScreen(
 
 //    }
     Column(
-        modifier = modifier,
+        modifier = modifier.verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
